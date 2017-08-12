91a03bf0e7cf49770104b2d3c408310e01deb27f
Konstantin Dukakis <dukakisxyz@users.noreply.github.com>
2017-08-12 21:43:36 +0200

    bug fixes

--- soil_monitor.py
+++ soil_monitor.py
@@ -14,12 +14,15 @@ GPIO.setup(pin,GPIO.IN)
 
 def callback(pin):  
 	if GPIO.input(pin):
-		print "Moist"
+		print ("Dry")
 	else:
-		print "Dry"
+		print ("Moist")
 
 # This line tells our script to keep an eye on our gpio pin and let us know when the pin goes HIGH or LOW
 GPIO.add_event_detect(18, GPIO.BOTH, bouncetime=300)
 # This line asigns a function to the GPIO pin so that when the above line tells us there is a change on the pin, run this function
 GPIO.add_event_callback(18, callback)
 
+while True:
+	# This line simply tells our script to wait 0.1 of a second, this is so the script doesnt hog all of the CPU
+	sleep(2)
