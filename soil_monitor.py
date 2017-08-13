ec063797bfb42779dd122cfb680cbb9d6947cd61
Konstantin Dukakis <dukakisxyz@users.noreply.github.com>
2017-08-13 11:54:17 +0200

    improved logic

--- soil_monitor.py
+++ soil_monitor.py
@@ -9,28 +9,16 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(pin,GPIO.IN)
 
-
-# Start loop
-while True: 
+def measurement(pin):  
 	if (GPIO.input(pin) == True):
 		print('3.3V – Dry soil')
 	else:
 		print('0V – Moist soil')
-	sleep(1);
-
-"""
-def callback(pin):  
-	if GPIO.input(pin):
-		print ("State 1")
-	else:
-		print ("State 2")
 
 # This line tells our script to keep an eye on our gpio pin and let us know when the pin goes HIGH or LOW
 GPIO.add_event_detect(18, GPIO.BOTH, bouncetime=300)
 # This line asigns a function to the GPIO pin so that when the above line tells us there is a change on the pin, run this function
-GPIO.add_event_callback(18, callback)
+GPIO.add_event_callback(18, measurement)
 
 while True:
 	sleep(2)
-"""
-
